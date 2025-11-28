dcbf296c7d3d531d67ba298594bd7ca844bd7175
Puntawat Subhamani <puntawatsub@gmail.com>
2025-11-28 11:21:01 +0200

    handle empty job list

--- frontend-simplified/src/components/JobListings.jsx
+++ frontend-simplified/src/components/JobListings.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import JobListing from './JobListing';
-import Spinner from './Spinner';
+import { useState, useEffect } from "react";
+import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -8,13 +8,13 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
-        console.log('Error fetching data', error);
+        console.log("Error fetching data", error);
       } finally {
         setLoading(false);
       }
@@ -23,25 +23,26 @@ const JobListings = ({ isHome = false }) => {
     fetchJobs();
   }, []);
 
-
   return (
-    <section className='bg-blue-50 px-4 py-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
 
         {loading ? (
           <Spinner loading={loading} />
         ) : (
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobs.map((job) => (<JobListing key={job.id} job={job} /> ))}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.length === 0 ? (
+              <p>No jobs available at the moment.</p>
+            ) : (
+              jobs.map((job) => <JobListing key={job.id} job={job} />)
+            )}
           </div>
         )}
       </div>
     </section>
   );
-
-  
 };
 export default JobListings;
