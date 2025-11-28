ed415db920649e3663fec1f653dd2bf90ba1e6e4
Swostika Lama <Swostika.Lama@metropolia.fi>
2025-11-28 11:53:47 +0200

    [iter2] updated from api fake to api starter

--- backend/api-server-starter/routes/jobRouter.js
+++ backend/api-server-starter/routes/jobRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../middleware/requireAuth')
+const auth = require('../middleware/authMiddleware')
 
 const {
   getAllJobs,
