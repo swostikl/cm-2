5b94f06a5aeffb9fc53fc630da5c25144eb69faf
Jiya <jiya.kanhirathanpoyil@metropolia.fi>
2025-11-28 10:50:08 +0200

    [iter2] add jobrouter

---
+++ backend/api-fake-server/routes/jobRouter.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../middleware/requireAuth')
+
+const {
+  getAllJobs,
+  getJobById,
+  createJob,
+  updateJobById,
+  deleteJobById,
+} = require('../controllers/jobController')
+
+router.use(auth)
+
+router.get('/', getAllJobs)
+router.post('/', createJob)
+router.get('/:jobId', getJobById)
+router.put('/:jobId', updateJobById)
+router.delete('/:jobId', deleteJobById)
+
+module.exports = router
