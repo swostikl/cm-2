3569aedf67225a477eaa52d348980cf7e6c83e8f
Swostika Lama <Swostika.Lama@metropolia.fi>
2025-11-28 15:05:42 +0200

    [Iter4]Final Tweaks and Optional Enhancements  explain the code and added in README.md

--- backend/api-server-starter/models/jobModel.js
+++ backend/api-server-starter/models/jobModel.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
 
-const jobSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  type: { type: String, required: true },
-  location: { type: String, required: true },
-  description: { type: String, required: true },
-  salary: { type: String, required: true },
-  company: {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    contactEmail: { type: String, required: true },
-    contactPhone: { type: String, required: true }
-  }
-});
+    const companySchema = new mongoose.Schema({
+      name: { type: String, required: true },
+      description: { type: String, required: true },
+      contactEmail: { type: String, required: true },
+      contactPhone: { type: String, required: true }
+    });
 
-// Ensure virtual fields are serialized
-jobSchema.set('toJSON', {
-  virtuals: true,
-  transform: (doc, ret) => {
-    ret.id = ret._id;
-    delete ret._id;
-    delete ret.__v;
-    return ret;
-  }
-});
+    const jobSchema = new mongoose.Schema({
+      title: { type: String, required: true },
+      type: { type: String, required: true },
+      location: { type: String, required: true },
+      description: { type: String, required: true },
+      salary: { type: String, required: true },
+      company: { type: companySchema, required: true }
+    });
 
-const Job = mongoose.model('Job', jobSchema);
+    jobSchema.set('toJSON', {
+    virtuals: true,
+    transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+    }
+    });
 
-module.exports = Job;
-
+    const Job = mongoose.model('Job', jobSchema);
+    module.exports = Job;  
